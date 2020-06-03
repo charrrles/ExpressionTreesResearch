637803dfd37c99810dfe57a7113865271ac3bc9f
Charles <charrrles@gmail.com>
2020-06-03 09:51:01 -0400

    Cleaned up: ContainerMemberHoldsAttribute complete.

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -24,8 +24,6 @@ namespace NUnitTestProject1
         public List<TestAttr> Attributes { get; set; }
     }
 
-
-
     public class Tests
     {
         IQueryable<TestObject> TestData { get; set; }
@@ -53,17 +51,6 @@ namespace NUnitTestProject1
             TestData = x.AsQueryable();
         }
 
-        //static Expression BuildAny<TSource>(Expression<Func<TSource, bool>> predicate)
-        //{
-        //    var overload = typeof(Queryable).GetMethod("Any");
-        //                            //  .Single(mi => mi.GetParameters().Count() == 2);
-        //    var call = Expression.Call(
-        //        overload,
-        //        Expression.PropertyOrField(projParam, "GroupsAssigned"),
-        //        predicate);
-
-        //    return call;
-        //}
         static Expression BuildAny<TSource>(ParameterExpression param, Expression<Func<TSource, bool>> predicate)
         {
             var overload = typeof(Queryable).GetMethods()
@@ -74,19 +61,6 @@ namespace NUnitTestProject1
                 predicate);
 
             return call;
-        }
-
-        static Expression<Func<IEnumerable<TSource>, bool>> CreateLambdaForAny2<TSource>(Expression<Func<TSource, bool>> predicate)
-        {
-            var source = Expression.Parameter(typeof(IEnumerable<TSource>), "source");
-            var overload = typeof(Queryable).GetMethods()
-                                      .Single(mi => mi.Name == "Any" && mi.GetParameters().Count() == 2);
-            var call = Expression.Call(
-                overload,
-                source,
-                predicate);
-
-            return Expression.Lambda<Func<IEnumerable<TSource>, bool>>(call, source);
         }
 
         static Expression<Func<IEnumerable<TSource>, bool>> CreateLambdaForAny<TSource>(Expression<Func<TSource, bool>> predicate)
@@ -100,107 +74,61 @@ namespace NUnitTestProject1
             return Expression.Lambda<Func<IEnumerable<TSource>, bool>>(call, source);
         }
 
-        Func<IEnumerable<IAttribute>, bool> BuildAttributesHoldAttrLambda<TSource>( IAttribute targetAttr)
+        Func<IEnumerable<IAttribute>, bool> BuildContainerHoldsAttributeLambda<TSource>( IAttribute targetAttr)
         {
             var anyLambdaExpr = CreateLambdaForAny<IAttribute>(p => p.Value == targetAttr.Value && p.Name == targetAttr.Name);
             return anyLambdaExpr.Compile();
-            //var f = Expression.Lambda<Func<IEnumerable<TSource>, bool>>(
-            //    anyLambdaExpr)
-            //    .Compile();
-            //return f;
         }
 
-
-        static Func<TSource, bool> BuildMemberHasAttributeLambda<TSource>(string fieldName, IAttribute targetAttr)
+        static Func<TSource, bool> BuildContainerMemberHoldsAttributeLambda<TSource>(string fieldName, IAttribute targetAttr)
         {
             var EntityParam = Expression.Parameter(typeof(TSource), "entity");
 
             var fieldExpression = Expression.PropertyOrField(EntityParam, fieldName);
 
-            Func<TSource, IEnumerable<IAttribute>> accessAttrsMember = Expression.Lambda<Func<TSource, IEnumerable<IAttribute>>>(fieldExpression, EntityParam).Compile();
-
-
-            Func<IEnumerable<IAttribute>, bool> anyLambda = CreateLambdaForAny<IAttribute>(p => p.Value == targetAttr.Value && p.Name == targetAttr.Name).Compile();
-
-
+            Func<TSource, IEnumerable<IAttribute>> accessAttrsMember = 
+                Expression.Lambda<Func<TSource, IEnumerable<IAttribute>>>(fieldExpression, EntityParam).Compile();
+            Func<IEnumerable<IAttribute>, bool> anyLambda = 
+                CreateLambdaForAny<IAttribute>(p => p.Value == targetAttr.Value && p.Name == targetAttr.Name).Compile();
 
             return T => anyLambda(accessAttrsMember(T));
-
-
-            /*
-            var call = Expression.Lambda(
-                anyLambdaExpr,
-                fieldExpression);/*/
-
-            //  var overload = typeof(Queryable).GetMethods()
-            //                           .Single(mi => mi.Name == "Any" && mi.GetParameters().Count() == 2);
-
-            //  .Single(mi => mi.GetParameters().Count() == 2);
-
-            // Expression<Func<IAttribute, bool>> predicate = p => p.Value == targetAttr.Value && p.Name == targetAttr.Name;
-
-            //var call = Expression.Call(
-            //    overload,
-            //    fieldExpression,
-            //    predicate);
-
-            /*   Func<TSource, bool> f = Expression.Lambda<Func<TSource, bool>>(
-                   call)
-                   .Compile();*/
-
-    
-
-            /*
-
-
-
-            return call;
-
-            var call = Expression.Call(
-                Expression.PropertyOrField(EntityParam, fieldName),
-                overload,
-                value);
-
-            return call;
-            */
-            //var value = Expression.Constant(attrValue);
-
-            //var memberConverted = Expression.Convert(member, value.Type);
-
-            //Expression equal = Expression.Equal(memberConverted, value);
-
-            //Func<object, bool> f = Expression.Lambda<Func<dynamic, bool>>(
-            //    equal,
-            //    param)
-            //    .Compile();
-
-            //return f;
-
         }
 
-
         [Test]
-        public void Test1()
+        public void ContainerHoldsAttribute()
         {
             TestAttr targetAttr = new TestAttr() { Name = "A1", Value = "33" };
-            var f = BuildAttributesHoldAttrLambda<TestAttr>(targetAttr);
+            var containerHolds_A1_33 = BuildContainerHoldsAttributeLambda<TestAttr>(targetAttr);
+
+            int hits = 0;
             foreach (var t in TestData)
             {
-                Console.WriteLine("manual: " + t.Attributes.Any(a => a.Name == targetAttr.Name && a.Value == targetAttr.Value));
-                Console.WriteLine("expression: " + f(t.Attributes));
+                Console.WriteLine("Manual: " + t.Attributes.Any(a => a.Name == targetAttr.Name && a.Value == targetAttr.Value));
+
+                var result = containerHolds_A1_33(t.Attributes);
+                Console.WriteLine("Expression: " + result);
+                if (result)
+                {
+                    hits++;
+                }
             }
 
-            var l = TestData.Where(td => td.Attributes.Any(a => a.Name == targetAttr.Name && a.Value == targetAttr.Value));
+            Assert.AreEqual(1, hits);
+        }
 
-            var EntityHasAttribute_A1_33 = BuildMemberHasAttributeLambda<TestObject>("Attributes", targetAttr);
+        [Test]
+        public void ContainerMemberHoldsAttribute()
+        {
+            TestAttr targetAttr = new TestAttr() { Name = "A1", Value = "33" };
+            var toEmulate = TestData.Where(td => td.Attributes.Any(a => a.Name == targetAttr.Name && a.Value == targetAttr.Value));
 
-            var l2 = TestData.Where(e => EntityHasAttribute_A1_33(e));
+            var EntityHasAttribute_A1_33 = BuildContainerMemberHoldsAttributeLambda<TestObject>("Attributes", targetAttr);
 
-            Console.WriteLine("Found object with targetAttr? " + TestData.Any(d => EntityHasAttribute_A1_33(d)));
+            var result = TestData.Where(e => EntityHasAttribute_A1_33(e));
 
-            Assert.IsTrue(TestData.Any(d => EntityHasAttribute_A1_33(d)));
+            Console.WriteLine("Found object with targetAttr? " + result.Any());
 
-
+            Assert.IsTrue(result.Count() == 1);
         }
     }
 }
